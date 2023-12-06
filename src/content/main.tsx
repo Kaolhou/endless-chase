ba592e5606431693e1dc4fb0bb86e160c79cf8ff
André Mendes <andre.mendes.rocha65@gmail.com>
2023-12-06 20:22:21 -0300

    resize

--- src/content/main.tsx
+++ src/content/main.tsx
@@ -1,10 +1,31 @@
 import GlitchedButton from "../components/glitched_button";
 import logo from "../assets/icons/image.webp";
+import { useEffect, useState } from "react";
 
 interface MainProps {
   scrollTop: number;
 }
 export default function Main({ scrollTop }: MainProps) {
+  const [dimensions, setDimensions] = useState<{
+    width: number;
+    height: number;
+  }>({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+  useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <section
       style={{
@@ -18,7 +39,9 @@ export default function Main({ scrollTop }: MainProps) {
       }}
       id="home"
     >
-      <div style={{ width: "100%" }}>
+      <div
+        style={{ width: "100%", opacity: dimensions.width < 880 ? "0" : "1" }}
+      >
         <img src={logo} alt="logo" style={{ width: "100%" }} />
       </div>
       <div style={{ display: "flex", justifyContent: "center", gap: 20 }}>
